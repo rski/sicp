66b80b933dc2840002e106230422c897132aa28d
Romanos Skiadas <rom.skiad@gmail.com>
2016-10-19 22:40:29 +0300

    Fix accidentally trying to apply f in compose else

--- ch1/procedures_as_returned_values.scm
+++ ch1/procedures_as_returned_values.scm
@@ -36,8 +36,6 @@
 (define (repeated f n)
   (cond ((< n 1) (lambda (x) x))
         ((= n 1) f)
-        (else  (compose (f
-                         (repeated f (- n 1))
-                         )))))
+        (else (compose f (repeated f (- n 1))))))
 
-((repeated (lambda (x) (+ 1 x) ) 6) 5)
+((repeated (lambda (x) (+ 1 x)) 6) 5)

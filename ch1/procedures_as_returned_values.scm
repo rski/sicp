6c9518eba0dc81603943319d146057ea043fa7e0
Romanos Skiadas <rom.skiad@gmail.com>
2016-10-19 22:19:08 +0300

    Ch1: Cubic, double, compose, repeated, smoothen

---
+++ ch1/procedures_as_returned_values.scm
@@ -0,0 +1,43 @@
+(define dx 0.001)
+(define (smoothen f)
+  (lambda (x) (/ (+ (f (- x dx))
+                    (f x)
+                    (f (+ x dx)))
+                 3)))
+
+(define (derivative f)
+  (lambda (x) (/ (- (f (+ x dx)) (f x))
+                 dx
+                 )))
+
+
+(define (newton-transform f)
+  (lambda (x) (- x
+                 (/ (f x)
+                    ((derivative f) x)))))
+
+(define (netwons-method f guess)
+  (fixed-point (neutron-transform f) guess))
+
+(define (cubic a b c)
+  (lambda (x) (+ (* x x x)
+                 (* x x a)
+                 (* x b)
+                 c)))
+
+(define (double f)
+  (lambda (x) (f (f x))))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+((double (lambda (x) (+ 1 x))) 3)
+
+(define (repeated f n)
+  (cond ((< n 1) (lambda (x) x))
+        ((= n 1) f)
+        (else  (compose (f
+                         (repeated f (- n 1))
+                         )))))
+
+((repeated (lambda (x) (+ 1 x) ) 6) 5)
